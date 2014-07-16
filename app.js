6be2fb9a06c8ae7acf9111044cb70a83e57a7f1a
Oliver Rogers <ollie@Olivers-MacBook-Pro.local>
2014-07-16 14:41:27 +0100

    update express

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
  * Module dependencies
  */
 var express = require( 'express' ),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    logger = require('morgan'),
     stylus = require( 'stylus' ),
-    nib = require( 'nib' ), 
+    nib = require( 'nib' ),
     path = require('path'),
+    flash = require('express-flash'),
+    nodemailer = require("nodemailer"),
     app = express( );
 
 
@@ -13,32 +20,58 @@ var express = require( 'express' ),
 /*
 Express settings
 */
-app.set( 'port', process.env.PORT || 3000);        // set port
+app.set( 'port', process.env.PORT || 8080);        // set port
 app.set( 'views', path.join(__dirname, 'views'));  // set views location
 app.set( 'view engine', 'jade');                   // set jade as view engine
-app.use(express.bodyParser());                     // use body parser to break down requests
-app.use(express.errorHandler());                   // use express' pretty error handling
-
+app.use(bodyParser( ));                            // use body parser to break down requests
+app.use(errorHandler());                           // use express' pretty error handling
 if (process.env.NODE_ENV === 'development'){       // log requests to console in development
-  app.use(express.logger('dev'));   
+  app.use(logger('dev'));
 }
-// stylus compile  settings
+
+
+// stylus settings
 function compile(str, path) {
+
+  var lineNos,
+      compressCss;
+
+  if(process.env.NODE_ENV === 'development') {
+      lineNos = true;
+      compressCss = false;
+  } else {
+      lineNos = false;
+      compressCss = true;
+  }
+
   return stylus(str)
     .set('filename', path)
-    .set('linenos', true)
-    .set('compress', false) 
+    .set('linenos', lineNos)
+    .set('compress', compressCss)
     .use(nib());
+
 }
-// use stylus
 app.use(stylus.middleware(
-  { 
+  {
     src: __dirname + '/public',
     compile: compile
   }
 ));
+
+
 // tidy markup
-app.locals.pretty = true;
+app.locals.pretty = false;
+
+
+// express-flash message settings
+app.use(cookieParser('keyboard cat'));
+app.use(session({
+  secret: 'add a phrase here',
+  maxAge: 60000,
+  cookie: { secure: true }
+}));
+app.use(flash());
+
 app.use(express.static( __dirname + '/public') );  // location for static files
 
 
@@ -51,9 +84,9 @@ app.get('/', function (req, res){
   });
 });
 
-// routes hack
+// temporary routes hack
 app.get('/:page', function (req, res){
-  var page = req.params.page; 
+  var page = req.params.page;
   res.render(page, {
     title: page
   });
